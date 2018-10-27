c3ac7bbd4d3337889bb1bdba209ba2b72f8ae17a
Lorrayne Paraiso Carneiro Flor <lorrayneparaisoc@gmail.com>
2018-10-27 00:39:20 -0300

    Make project

---
+++ MarvelHeroes/MarvelHeroes/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MarvelHeroes
+//
+//  Created by Lorrayne Paraiso  on 27/10/18.
+//  Copyright © 2018 Lorrayne Paraiso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
---
+++ MarvelHeroes/MarvelHeroesUITests/MarvelHeroesUITests.swift
@@ -0,0 +1,27 @@
+//
+//  MarvelHeroesUITests.swift
+//  MarvelHeroesUITests
+//
+//  Created by Lorrayne Paraiso  on 27/10/18.
+//  Copyright © 2018 Lorrayne Paraiso. All rights reserved.
+//
+
+import XCTest
+
+class MarvelHeroesUITests: XCTestCase {
+
+    override func setUp() {
+        continueAfterFailure = false
+
+        XCUIApplication().launch()
+    }
+
+    override func tearDown() {
+
+    }
+
+    func testExample() {
+
+    }
+
+}
